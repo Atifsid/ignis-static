cb525850c300f9b4c47bb2552f68267ab61c0c9a
Atifsid <atifsiddiqui35@gmail.com>
2024-08-07 23:45:39 +0530

    chore: create loading-overlay

---
+++ script.js
@@ -0,0 +1,6 @@
+gsap.to("#loading-overlay", {
+  y: "100%",
+  delay: 2,
+  duration: 0.5,
+  ease: "power.inOut",
+});
--- script/navbar.js
+++ script/navbar.js
@@ -1,37 +1,40 @@
 let mm = gsap.matchMedia();
 let navbarTl = gsap.timeline();
-const menuBtn = document.querySelector('.navbar span i');
-const crossBtn = document.querySelector('#navbar-full i');
+const menuBtn = document.querySelector(".navbar span i");
+const crossBtn = document.querySelector("#navbar-full i");
 
-gsap.from('.logo', {
-    duration: 1,
-    x: -100,
-})
-gsap.from('.nav-links', {
-    duration: 1,
-    x: 100,
-    stagger: 1,
-})
-gsap.from('.hero .hero-content', {
-    duration: 1,
-    scale: 2,
-    stagger: 1
-})
+gsap.from(".logo", {
+  delay: 2,
+  duration: 1,
+  x: -100,
+});
+gsap.from(".nav-links", {
+  delay: 2,
+  duration: 1,
+  x: 100,
+  stagger: 1,
+});
+gsap.from(".hero .hero-content", {
+  delay: 2,
+  duration: 1,
+  scale: 2,
+  stagger: 1,
+});
 
 mm.add("(max-width: 750px)", () => {
-    navbarTl.to('#navbar-full', {
-        right: 1,
-        duration: 0.5,
-        display: "block"
-    });
-})
+  navbarTl.to("#navbar-full", {
+    right: 1,
+    duration: 0.5,
+    display: "block",
+  });
+});
 
 navbarTl.pause();
 
-menuBtn.addEventListener('click', () => {
-    navbarTl.play();
-})
+menuBtn.addEventListener("click", () => {
+  navbarTl.play();
+});
 
-crossBtn.addEventListener('click', () => {
-    navbarTl.reverse();
-})
+crossBtn.addEventListener("click", () => {
+  navbarTl.reverse();
+});
