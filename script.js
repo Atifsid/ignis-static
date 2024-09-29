214bf4e939ee4b5358ab4f35fa79af1a266b6de9
Atifsid <atifsiddiqui35@gmail.com>
2024-09-29 09:47:32 +0530

    chore: create modal for remaining services

--- script.js
+++ script.js
@@ -7,11 +7,267 @@ gsap.to("#loading-overlay", {
   ease: "power.inOut",
 });
 
-const ffs = document.getElementById('FFS');
-ffs.innerHTML = `<div class="modal-dialog modal-lg" role="document">
+const FFS = document.getElementById('FFS');
+FFS.innerHTML = `<div class="modal-dialog modal-lg" role="document">
   <div class="modal-content">
     <div class="modal-header">
       <h5 class="modal-title" id="exampleModalLabel">Fire Fighting System</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body center-content">
+      <ul>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/0.jpg' alt='Hydrant System'>
+              <figcaption>Hydrant System</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/1.jpg' alt='Hydrant System'>
+              <figcaption>Hydrant System</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/2.jpg' alt='MVWS'>
+              <figcaption>MVWS</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/3.jpg' alt='Watermist'>
+              <figcaption>Watermist</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/4.jpg' alt='Watermist'>
+              <figcaption>Watermist</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/5.jpg' alt='FM200 &amp; Novec gas suppression system'>
+              <figcaption>FM200 &amp; Novec gas suppression system</figcaption>
+            </figure>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>`;
+
+const FSS = document.getElementById('FSS');
+FSS.innerHTML = `<div class="modal-dialog modal-lg" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">Fire Suppression System</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body center-content">
+      <ul>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/5.jpg' alt='Hydrant System'>
+              <figcaption>Hydrant System</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/6.jpg' alt='Hydrant System'>
+              <figcaption>Hydrant System</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/7.jpg' alt='MVWS'>
+              <figcaption>MVWS</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/8.jpg' alt='Watermist'>
+              <figcaption>Watermist</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/9.jpg' alt='Watermist'>
+              <figcaption>Watermist</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/10.jpg' alt='FM200 &amp; Novec gas suppression system'>
+              <figcaption>FM200 &amp; Novec gas suppression system</figcaption>
+            </figure>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>`;
+
+const FAS = document.getElementById('FAS');
+FAS.innerHTML = `<div class="modal-dialog modal-lg" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">Fire Alarm System</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body center-content">
+      <ul>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/10.jpg' alt='Hydrant System'>
+              <figcaption>Hydrant System</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/12.jpg' alt='Hydrant System'>
+              <figcaption>Hydrant System</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/2.jpg' alt='MVWS'>
+              <figcaption>MVWS</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/3.jpg' alt='Watermist'>
+              <figcaption>Watermist</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/4.jpg' alt='Watermist'>
+              <figcaption>Watermist</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/5.jpg' alt='FM200 &amp; Novec gas suppression system'>
+              <figcaption>FM200 &amp; Novec gas suppression system</figcaption>
+            </figure>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>`;
+
+const RHT = document.getElementById('RHT');
+RHT.innerHTML = `<div class="modal-dialog modal-lg" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">Refilling & Hydro Test Service</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body center-content">
+      <ul>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/0.jpg' alt='Hydrant System'>
+              <figcaption>Hydrant System</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/1.jpg' alt='Hydrant System'>
+              <figcaption>Hydrant System</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/2.jpg' alt='MVWS'>
+              <figcaption>MVWS</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/3.jpg' alt='Watermist'>
+              <figcaption>Watermist</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/4.jpg' alt='Watermist'>
+              <figcaption>Watermist</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">
+            <figure>
+              <img src='/assets/5.jpg' alt='FM200 &amp; Novec gas suppression system'>
+              <figcaption>FM200 &amp; Novec gas suppression system</figcaption>
+            </figure>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>`;
+
+const AMS = document.getElementById('AMS');
+AMS.innerHTML = `<div class="modal-dialog modal-lg" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">Annual Maintenance Service</h5>
       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
